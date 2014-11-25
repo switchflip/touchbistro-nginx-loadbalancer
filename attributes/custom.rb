53231f180da38e3626c927dcf4f1ed7e1fff2095
Thomas Berry <thom.berry@gmail.com>
2014-11-25 12:54:44 -0500

    using nginx 1.6.1, ssl_buffer_size now working

--- attributes/custom.rb
+++ attributes/custom.rb
@@ -1,4 +1,11 @@
 # === Nginx General Configuration ===
+override['nginx']['version'] = '1.6.1'
+override['nginx']['source']['checksum'] = '943ad757a1c3e8b3df2d5c4ddacc508861922e36fa10ea6f8e3a348fc9abfc1a'
+
+# Reload nginx::source attributes with our updated version
+node.from_file(run_context.resolve_attribute('nginx', 'source'))
+
+
 normal['nginx']['install_method']             = 'source'
 normal['nginx']['init_style']                 = 'upstart'
 normal['nginx']['default_site_enabled']       = false
