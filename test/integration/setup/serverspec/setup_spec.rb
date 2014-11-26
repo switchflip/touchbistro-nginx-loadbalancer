891dd12b5a6888b135fa346f9a2f35f193af5e22
Thomas Berry <thom.berry@gmail.com>
2014-11-26 16:29:56 -0500

    Passing integration tests for certs realted to DHparams and OCSP stapling

--- test/integration/setup/serverspec/setup_spec.rb
+++ test/integration/setup/serverspec/setup_spec.rb
@@ -32,12 +32,14 @@ describe file('/etc/nginx') do
   it { should be_directory }
   it { should be_mode 755 }
   it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
 end
 
 describe file('/etc/nginx/ssl') do
   it { should be_directory }
   it { should be_mode 700 }
   it { should be_owned_by 'nginx' }
+  it { should be_grouped_into 'nginx' }
 end
 
 ssl_keys.each do |k| 
@@ -45,6 +47,7 @@ ssl_keys.each do |k|
     it { should be_file }
     it { should be_mode 600 }
     it { should be_owned_by 'nginx' }
+    it { should be_grouped_into 'nginx' }
   end
 end
 
@@ -53,6 +56,7 @@ nginx_confs.each do |conf|
     it { should be_file }
     it { should be_mode 744 }
     it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
   end
 end
 
@@ -61,6 +65,20 @@ describe file '/etc/nginx/ssl/dhparam.pem' do
   it { should be_mode 644 }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
+end
+
+describe file('/etc/ssl/private') do
+  it { should be_directory }
+  it { should be_mode 700}
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root'}
+end 
+
+describe file('/etc/ssl/private/ca-certs.pem') do
+  it { should be_file}
+  it { should be_mode '644' }
+  it { should be_owned_by 'root'}
+  it { should be_grouped_into 'root'}
 end
 
 describe service('nginx') do
