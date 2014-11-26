67bc728cb829ff9208835838f5fcf9d29af2d5a5
Thomas Berry <thom.berry@gmail.com>
2014-11-26 15:45:18 -0500

    Adding integration tests for setup.rb

--- test/integration/setup/serverspec/setup_spec.rb
+++ test/integration/setup/serverspec/setup_spec.rb
@@ -1,13 +1,9 @@
 require 'serverspec'
 set :backend, :exec
 
-describe service('nginx') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-# Users testing
+packages       = ['tar', 'vim']
 non_root_users = ['vagrant','nginx']
+ssl_keys       = ['nginx-test.crt', 'nginx-test.key']
 
 non_root_users.each do |u|
   describe user(u) do
@@ -23,4 +19,42 @@ describe user('root') do
   it { should have_uid 0 }
   it { should have_home_directory '/root' }
   it { should have_login_shell '/bin/bash' }
+end
+
+packages.each do |p| 
+  describe package p do
+    it { should be_installed }
+  end
+end
+
+describe file('/etc/nginx') do
+  it { should be_directory }
+  it { should be_mode 755 }
+  it { should be_owned_by 'root' }
+end
+
+
+describe file('/etc/nginx/ssl') do
+  it { should be_directory }
+  it { should be_mode 700 }
+  it { should be_owned_by 'nginx' }
+end
+
+ssl_keys.each do |k| 
+  describe file("/etc/nginx/ssl/#{k}") do
+    it { should be_file }
+    it { should be_mode 600 }
+    it { should be_owned_by 'nginx' }
+  end
+end
+
+
+# LAST
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(8443) do
+  it { should_not be_listening }
 end
